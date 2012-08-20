b8f3aa268ce479ccc3525b5c2444e2a9870f9696
Nathan Oehlman <nathan.oehlman@sidelab.com>
2012-08-20 15:38:23 +1000

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+var Proxy = require('nginx-proxy');
+
+function NginxProxyConfigurer(config, instance) {
+    
+    if (!config || !config.nginx) {
+        throw new Error('No nginx path available. Add nginx to steelmesh configuration');
+    }
+    
+    if (!instance) {
+        throw new Error('No instance provided - proxy returning');
+    }
+    
+    this.nginx = new Proxy(config.nginx);
+    this.config = config;
+    
+    instance.on('message.steelmesh.client.up', function(message, handle) {
+        console.log('Registering new application instance with nginx');
+        
+        instance.on('exit', function() {
+            console.log('Removing application instance from nginx');
+        })
+    });
+    
+}
+
+// Export the plugin
+module.exports = function(config, instance) {
+    return new NginxProxyConfigurer(config, instance);
+}
